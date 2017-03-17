1058c7b5ac588b85d6e91ab88d2dad7c81ee8903
916253 <916253@users.noreply.github.com>
2017-03-17 01:13:33 -0400

    run.py cleanup (Thanks to Aurora Wright)

--- run.py
+++ run.py
@@ -13,14 +13,11 @@ Illyasveil, the basic selfbot that doesn't do much yet.
 # import dependencies
 import os
 from discord.ext import commands
-import discord
 import datetime, re
-import json, asyncio
-import copy
+import json
+import discord
+import asyncio
 import configparser
-import traceback
-import sys
-import os
 
 # sets working directory to bot's folder
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -30,32 +27,14 @@ os.chdir(dir_path)
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-
-
 prefix = [',']
 bot = commands.Bot(command_prefix=prefix, description=description, pm_help=None, self_bot=True)
-
-
-
-# http://stackoverflow.com/questions/3411771/multiple-character-replace-with-python
-chars = "\\`*_<>#@:"
-def escape_name(name):
-    name = str(name)
-    for c in chars:
-        if c in name:
-            name = name.replace(c, "\\" + c)
-    return name.replace("@", "@\u200b")  # prevent mentions
-bot.escape_name = escape_name
-
-bot.pruning = False 
 
 @bot.event
 async def on_ready():
     for server in bot.servers:
         print("{} has started! {} has {:,} members!".format(bot.user.name, server.name, server.member_count))
-        bot.server = server
     await bot.change_presence(status=discord.Status.offline)
-
 
 # loads extensions
 addons = [
@@ -64,14 +43,11 @@ addons = [
     'addons.playing',
 ]
 
-failed_addons = []
-
 for extension in addons:
     try:
         bot.load_extension(extension)
     except Exception as e:
         print('{} failed to load.\n{}: {}'.format(extension, type(e).__name__, e))
-        failed_addons.append([extension, type(e).__name__, e])
 
 # Execute
 print('Bot directory: ', dir_path)
