b717324e5799a0e54ed85697a0334cf57bcb0e85
Mikaela RJ Szekely <qyriad@gmail.com>
2018-02-11 03:22:02 -0500

    Add a bunch of meme stuff, mostly dynamic blobs

--- addons/memes.py
+++ addons/memes.py
@@ -6,6 +6,7 @@ import discord
 from discord.ext import commands
 import asyncio
 from sys import argv
+import os.path
 
 class Memes:
     """
@@ -14,11 +15,16 @@ class Memes:
     def __init__(self, bot):
         self.bot = bot
         print('Addon "{}" loaded'.format(self.__class__.__name__))
-
+  
     async def _meme(self, ctx, msg):
         author = ctx.message.author
         await self.bot.delete_message(ctx.message)
         await self.bot.say(msg)
+
+    async def relfi(self, ctx, filepath):
+        with open(os.path.expanduser(filepath), 'rb') as f:
+            await self.bot.send_file(ctx.message.channel, f)
+        await self.bot.delete_message(ctx.message)
 
     # list memes
     @commands.command(name="listmemes", pass_context=True)
@@ -52,6 +58,11 @@ class Memes:
         """Memes."""
         await self._meme(ctx, "http://i.imgur.com/bp2YRAf.png")
         
+    @commands.command(pass_context=True, hidden=True)
+    async def wat2(self, ctx):
+        """Memes."""
+        await self._meme(ctx, "https://i.imgur.com/7vrkIex.jpg")
+
     @commands.command(pass_context=True, hidden=True, name="no!!")
     async def nope(self, ctx):
         """Memes."""
@@ -71,6 +82,16 @@ class Memes:
     async def nya(self, ctx):
         """Memes."""
         await self._meme(ctx, "http://i.imgur.com/WKB08Iq.png")
+
+    @commands.command(pass_context=True, hidden=True)
+    async def emma(self, ctx):
+        """Memes."""
+        await self._meme(ctx, "https://i.imgur.com/NukslEg.png")
+    
+    @commands.command(pass_context=True, hidden=True)
+    async def aurora(self, ctx):
+        """Memes."""
+        await self._meme(ctx, "https://i.imgur.com/anWqsDx.png")
 
     @commands.command(pass_context=True, hidden=True, name="916253")
     async def numbers(self, ctx):
@@ -133,6 +154,16 @@ class Memes:
         await self._meme(ctx, "http://i.imgur.com/hki1IIs.gifv")
 
     # Cute commands :3
+    @commands.command(pass_context=True, hidden=True)
+    async def hug(self, ctx):
+        """Cute"""
+        await self._meme(ctx, "https://i.imgur.com/Xp451cr.gifv")
+    
+    @commands.command(pass_context=True, hidden=True)
+    async def hug2(self, ctx):
+        """Cute"""
+        await self._meme(ctx, "https://media.discordapp.net/attachments/374342393828474881/412137578087710740/unknown.png?width=842&height=474")
+    
     @commands.command(pass_context=True, hidden=True)
     async def headpat(self, ctx):
         """Cute"""
@@ -218,7 +249,65 @@ class Memes:
     @commands.command(pass_context=True, hidden=True)
     async def negativity(self, ctx):
         """:c"""
-        await self._meme(ctx, "hhttp://i.imgur.com/1D5vHSk.png")
+        await self._meme(ctx, "http://i.imgur.com/1D5vHSk.png")
+
+    @commands.command(pass_context=True, hidden=True)
+    async def facepalm(self, ctx):
+        """:c"""
+        await self._meme(ctx, "https://i.imgur.com/N2CTB9B.jpg")
+
+    @commands.command(pass_context=True, hidden=True)
+    async def facedesk(self, ctx):
+        """:c"""
+        await self._meme(ctx, "https://i.imgur.com/fJygWAi.gifv")
+
+    @commands.command(pass_context=True, hidden=True)
+    async def clap(self, ctx):
+        """:c"""
+        await self._meme(ctx, "https://i.imgur.com/I7MmPyf.gifv")
+
+    @commands.command(pass_context=True, hidden=True)
+    async def blobowo(self, ctx):
+        """blob"""
+        await self._meme(ctx, "https://i.imgur.com/8azxDEj.png")
+
+    @commands.command(pass_context=True, hidden=True)
+    async def blobsmileopenmouth2(self, ctx):
+        """blob"""
+        await self._meme(ctx, "https://i.imgur.com/6mU2CMo.png")
+
+    @commands.command(pass_context=True, hidden=True)
+    async def blobsmilehappy(self, ctx):
+        """blob"""
+        await self._meme(ctx, "https://i.imgur.com/YHDFEMB.png")
+
+    @commands.command(pass_context=True, hidden=True)
+    async def blobsnuggle(self, ctx):
+        """blob"""
+        await self.relfi(ctx, "~/blobs_named/blobsnuggle.png")
+    
+    @commands.command(pass_context=True, hidden=True)
+    async def blob(self, ctx, text: str):
+        """blob"""
+        await self.relfi(ctx, "~/blobs_named/blob" + text + ".png")
+
+    
+    @commands.command(pass_context=True, hidden=True)
+    async def idontneedit(self, ctx):
+        """mirai"""
+        await self._meme(ctx, "https://i.giphy.com/media/mfggBMdObcqKA/giphy.webp")
+    
+    @commands.command(pass_context=True, hidden=True)
+    async def waytb(self, ctx):
+        """mirai"""
+        await self._meme(ctx, "https://media.giphy.com/media/BPDJz77jRp5XW/giphy.gif")
+    
+    @commands.command(pass_context=True, hidden=True)
+    async def yukityping(self, ctx):
+        """yuki"""
+        await self._meme(ctx, "https://media.giphy.com/media/AceKHfcUrqauQ/giphy.gif")
+
+
 
 # Load the extension
 def setup(bot):
